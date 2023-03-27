dfc51efa3a594b9658cd54610275c8f80ac0e995
samu7861 <sfrancog7861@gmail.com>
2023-03-27 10:12:48 -0500

    develpo code to production code

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path")
 
 module.exports = {
@@ -5,5 +6,21 @@ module.exports = {
     output: {
         path: path.join(__dirname, "dist"),
         filename: "bundle.js"
-    }
+    },
+    devServer: {
+        port: 3000
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        })
+    ]
 };
